cd3a1a24c38a28917b9ad4bac4d2c6490063e8a3
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-02-01 21:17:12 +1300

    Begun home component

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar.js';
+import Home from './components/Home.js';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Home />
     </div>
   );
 }
---
+++ personal-portfolio/src/components/Home.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import '../assets/styles/Home.css';
+
+function Home() {
+    return (
+        <div className="Home">
+            <h1>ALEXANDER <br />HEFFERNAN</h1>
+            <h2>SOFTWARE ENGINEER, FRONT/BACK END & APP DEVELOPER</h2>
+        </div>
+    )
+}
+
+export default Home;
