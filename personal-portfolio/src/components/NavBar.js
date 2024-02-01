72319d557480bece603da36152e8e4de209bb6f8
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-02-01 20:54:21 +1300

    Finished first version of NavBar

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -1,15 +1,6 @@
 import React, { useState } from 'react';
 import '../assets/styles/NavBar.css';
 
-function myFunction() {
-    var navbar = document.getElementById("navbar");
-    if (navbar.className === "navbar") {
-      navbar.className += " responsive";
-    } else {
-      navbar.className = "navbar";
-    }
-  }
-
 function NavBar() {
     const [isMenuOpen, setMenuOpen] = useState(false);
 
@@ -21,12 +12,12 @@ function NavBar() {
         <div className={`navbar ${isMenuOpen ? 'open' : ''}`} id="navbar">
             <a className="title" href="#home">Alexander <br />Heffernan</a>
             <div className="links">
-                <a id="text" href="#about" className="active">About</a>
-                <a id="text" href="#work">Work</a>
-                <a id="text" href="#education">Education</a>
-                <a id="text" href="#portfolio">Portfolio</a>
-                <a id="text" href="#contact">Contact</a>
-                <a id="button" href="#resume"><span>Resume</span></a>
+                <a id="text" href="#about" className="active"><span class="num">1.</span> About</a>
+                <a id="text" href="#work"><span class="num">2.</span> Work</a>
+                <a id="text" href="#education"><span class="num">3.</span> Education</a>
+                <a id="text" href="#portfolio"><span class="num">4. </span>Portfolio</a>
+                <a id="text" href="#contact"><span class="num">5. </span>Contact</a>
+                <a id="button" href="#resume"><span><span>Resume</span></span></a>
             </div>
             <a className="icon" onClick={handleHamburgerClick}>
                 <i className="fa fa-bars"></i>
