be5cf4b752fac94b8e84842048947b5a481c351c
Alexander Heffernan <heffernan.alexander@icloud.com>
2024-02-09 16:14:51 +1300

    Round 1 of responsive personal testing

--- personal-portfolio/src/components/NavBar.js
+++ personal-portfolio/src/components/NavBar.js
@@ -12,12 +12,12 @@ function NavBar() {
         <div className={`navbar ${isMenuOpen ? 'open' : ''}`} id="navbar">
             <a className="title" href="#Home">Alexander <br />Heffernan</a>
             <div className="links">
-                <a id="text" href="#About" className="active"><span className="num">01.</span> About</a>
-                <a id="text" href="#Work"><span className="num">02.</span> Work</a>
-                <a id="text" href="#Education"><span className="num">03.</span> Education</a>
-                <a id="text" href="#Portfolio"><span className="num">04. </span>Portfolio</a>
-                <a id="text" href="#Contact"><span className="num">05. </span>Contact</a>
-                <a id="button" href="#Resume"><span><span>Resume</span></span></a>
+                <a onClick={handleHamburgerClick} id="text" href="#About" className="active"><span className="num">01.</span> About</a>
+                <a onClick={handleHamburgerClick} id="text" href="#Work"><span className="num">02.</span> Work</a>
+                <a onClick={handleHamburgerClick} id="text" href="#Education"><span className="num">03.</span> Education</a>
+                <a onClick={handleHamburgerClick} id="text" href="#Portfolio"><span className="num">04. </span>Portfolio</a>
+                <a onClick={handleHamburgerClick} id="text" href="#Contact"><span className="num">05. </span>Contact</a>
+                <a onClick={handleHamburgerClick} id="button" href="#Resume"><span><span>Resume</span></span></a>
             </div>
             <a className="icon" onClick={handleHamburgerClick}>
                 <i className="fa fa-bars"></i>
